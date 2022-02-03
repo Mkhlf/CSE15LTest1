9cb18cc588613344644f3244aa31e941451162a3
Mohammad Alkhalifah <56599163+Mkhlf@users.noreply.github.com>
2022-02-03 11:03:09 -0800

    Creating a failing test case

--- Javafile.java
+++ Javafile.java
@@ -1,5 +1,10 @@
 public class Javafile {
 
+    public static boolean returnFalse() {
+        return false;
+
+    }
+
     public static void main() {
 
     }
--- JavafileTest.java
+++ JavafileTest.java
@@ -1,9 +1,17 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+/**
+ * To run the file use:
+ * 
+ * javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar JavafileTest.java
+ * 
+ * java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore JavafileTest
+ */
 public class JavafileTest {
     @Test
     public void test1() {
+        assertTrue(Javafile.returnFalse());
 
     }
 
